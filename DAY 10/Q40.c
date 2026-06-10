39110694f50d3b0103eb5011f67e259be79c42b0
sakshi080201-hash <sakshi080201@gmail.com>
2026-06-10 21:36:16 +0530

    DAY 10 CODES

---
+++ DAY 10/Q40.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    for(i = 1; i <= rows; i++)
+    {
+        for(j = 1; j <= rows - i; j++)
+        {
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++)
+        {
+            printf("%c", 'A' + j - 1);
+        }
+        for(j = i - 1; j >= 1; j--)
+        {
+            printf("%c", 'A' + j - 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
