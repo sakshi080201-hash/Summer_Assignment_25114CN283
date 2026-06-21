3aa7407710f6be2c631ce0f45c8855125fa085e2
sakshi080201-hash <sakshi080201@gmail.com>
2026-06-21 22:23:14 +0530

    DAY 20 CODES

---
+++ DAY 20/Q80.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j, sum;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int a[rows][cols];
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Column-wise Sum:\n");
+    for(j = 0; j < cols; j++) {
+        sum = 0;
+        for(i = 0; i < rows; i++) {
+            sum += a[i][j];
+        }
+        printf("Sum of Column %d = %d\n", j + 1, sum);
+    }
+    return 0;
+}
